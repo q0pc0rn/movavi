fa0c8fbbd4a45cbd5db77a969f2ac05d8f0e6728
q0pc0rn <landogg@inbox.ru>
2017-10-09 12:13:51 +0700

    xs sm not not nav-tab css

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
     $('.swap_mode').click( function(){ // ловим клик по ссылке с классом go_to
-	var scroll_el = $(this).attr('toggle'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+	      var scroll_el = $(this).attr('toggle'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
         if ($(scroll_el).length != 0) {
-          // var swap_mode_class= $(this).attr('class');
-          // var a_href = $(scroll_el);
-          // console.log($(swap_mode_class));
-          // console.log($(a_href));
-          var a_class = $(this).find('a');
-          $(a_class).onclick();
-          console.log(a_class);
-
-            if ( $(swap_mode_class) === $(scroll_el)) {
-
-            };
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+          $(`#scroll a[href="${scroll_el}"]`).tab('show');
+	        $('html, body').animate({ scrollTop: $('#scroll').offset().top }, 500); // анимируем скроолинг к элементу scroll_el
         }
 	    return false; // выключаем стандартное действие
     });
 });
+ 
